39ee9f6b1ba94bbd80fbf6bf1032e402d94e2a0e
Kapeel Kokane <kapeel.kokane@gmail.com>
2017-10-14 22:07:35 +0530

    first commit with basic flesch algorithm

--- src/app/Main.js
+++ src/app/Main.js
@@ -9,11 +9,22 @@ import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
 
 const styles = {
   container: {
     textAlign: 'center',
-    paddingTop: 200,
+    marginTop: 200
+  },
+  main: {
+    textAlign: 'center',
+    width: 1000,
+    marginLeft: 200,
+    marginTop: 50
+  },
+  padd: {
+    padding: 20
   },
 };
 
@@ -32,45 +43,69 @@ class Main extends Component {
     };
   }
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  }
-
   handleTouchTap = () => {
     this.setState({
       open: true,
     });
   }
 
-  render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
+  calculateScore = (e) => {
+	const text = e.target.value;
 
+	let nWords = 0, nSentences = 0, nSyllabes = 0;
+	nWords = text.split(/[a-zA-z]+/).length-1;
+	nSentences = text.split(/[^!?.]+/).length-1;
+	nSyllabes = text.split(/[a-zA-Z]/).length-1;
+
+	console.log('words :', nWords, 'sentences :', nSentences, 'syllables :', nSyllabes);
+	let score = 206.835 - 1.015*(nWords/nSentences) - 84.6*(nSyllabes/nWords);
+
+	this.setState({score: score});
+  }
+
+  render() {
+
+    let startInstructions = null;
+    let mainContent = null;
+
+    if(!this.state.open) {
+      startInstructions = (
+        <div style={styles.container}>
+        <h1>Flesch Readability Analyzer</h1>
+        <h2>A material UI based project</h2>
+        <RaisedButton
+          label="Let's do it!"
+          secondary={true}
+          onTouchTap={this.handleTouchTap}
+        />
+      </div>
+      );
+    }
+
+    if(this.state.open) {
+      mainContent = (
+        <div style={styles.main}>
+          <AppBar
+            title="Flesch Readability Analyzer"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          <TextField 
+		  	style={styles.padd}
+            hintText="Type the paragraph or paste here to analyze flesch readability score"
+            fullWidth={true}
+            multiLine={true}
+            rows={15}
+            onChange={this.calculateScore}
+          />
+		  <h1> {this.state.score ? `Flesch Score : ${this.state.score}` : null}</h1>
+        </div>
+      );
+    }
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
+        <div>
+          {startInstructions}
+          {mainContent}
         </div>
       </MuiThemeProvider>
     );
